b3b7c5ee3aa5e519bfedf7d5f7cf17420c6a1867
Aidne46 <461676@bsd48.org>
2022-08-24 20:29:04 -0700

    revise

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
-self.addEventListener('push', () => {
- self.registration.showNotification('test', {});
-  
-}); 
+self.addEventListener('push', function(e) {
+    var options = {
+        body: 'This Notification was generated from a push',
+        icon: 'images/example.png',
+        vibrate: [100, 50, 100],
+        data: {
+            dateOfArrival: Date.now(),
+            primaryKey: '2'
+        },
+        actions: [
+            {
+             action: 'explore',
+             title: 'Explore this new world',
+             icon: 'images/checkmark.png'
+            },
+            { action: 'close', title: 'Close', icon: 'images/mark.png' }
+        ]
+    };
+    e.waitUntil(self.registration.showNotification('Hellow world!', options));
+});
